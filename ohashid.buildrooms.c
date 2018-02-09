51feba9327b5e14aac28878cd42563c491f92def
Dean Ohashi <ohashid@os1.engr.oregonstate.edu>
2018-02-09 10:44:59 -0800

    added comments to buildrooms

--- ohashid.buildrooms.c
+++ ohashid.buildrooms.c
@@ -17,7 +17,9 @@
 
 #define MAX_ROOMS 6
 
-
+/*
+ Declared global array to hold 10 possible room names to be randomly selected
+*/
 char* roomNames[] = {"Nursery", "BedOne", "BedTwo", "Living", "Kitchen", "Basement",
 			 "Attic", "Garage", "Bathroom", "Office"};
 
@@ -31,6 +33,9 @@ struct Room{
 	struct Room* connections[MAX_ROOMS];
 };
 
+/*
+ Declared globay array to hold seven rooms
+*/
 struct Room rooms[7];
 
 int IsGraphFull();
@@ -47,6 +52,8 @@ int roomUsed(char *string);
 */
 int roomUsed(char *string){
 	int i;
+	
+	//Iterate through all 7 rooms and compare their names to the argument string
 	for(i = 0; i < 7; i++){
 		if(rooms[i].roomName == string) return 1;
 	}
@@ -60,7 +67,8 @@ int roomUsed(char *string){
 int IsGraphFull(){
 	int i;
 	int bool = 1;
-
+	//iterate through all 7 rooms to check if their connections are less than 3
+	//if connections are less than 3, that given room can still be connected
 	for(i = 0; i < 7; i++){
 		if(rooms[i].numConnections < 3) bool = 0;
 	}
@@ -72,26 +80,25 @@ int IsGraphFull(){
  Gets two rooms, validates room, verifies they can get connected, and connects the two rooms.
 */
 void AddRandomConnection(){
+	//pointers to struct Rooms to get pointers to various rooms[]
 	struct Room* A;
 	struct Room* B;
+	//following while-loop to retrieve first room A, will continue to run until break statement
 	while(1){
-		printf("AddRandomConnection(): Retrieving A\n");
-		A = GetRandomRoom();
+		A = GetRandomRoom(); //retrieves random room
 		if(CanAddConnectionFrom(A) == 1){
-			printf("A: %s\n", A->roomName);
-			break;
+			break; //if an appropriate room is found, the loop is exited via break
 		}
 	}
-	printf("Retrieveing B\n");
+
 	do{
-		B = GetRandomRoom();
+		B = GetRandomRoom(); //similar to A, gets random room for struct Room* B
 	}while(CanAddConnectionFrom(B) == 0 || IsSameRoom(A, B) == 1 || ConnectionAlreadyExists(A, B) == 1);
+	//do-while continues to loop while the retrieved room cannot have additional connections
+	//is the same room as A, or already has a pre-existing connection with A
 
-	printf("out of the do while loop\n");
-
-	printf("A: %s\n", A->roomName);
-	printf("B: %s\n", B->roomName);
-	ConnectRoom(A, B);
+	//calls ConnectRooms() twice to link A--> B and B-->A
+	ConnectRoom(A, B); 
 	ConnectRoom(B, A);
 }
 
@@ -99,7 +106,7 @@ void AddRandomConnection(){
  Returns random room.
 */
 struct Room* GetRandomRoom(){
-	int roomNum = rand() % 7;
+	int roomNum = rand() % 7; //generates random integer between 0 ~ 6 (inclusive)
 	return &rooms[roomNum];
 }
 
@@ -108,8 +115,8 @@ struct Room* GetRandomRoom(){
  Returns 1 if possible, returns 0 if not
 */
 int CanAddConnectionFrom(struct Room* x){
-	int bool = 1;
-	if(x->numConnections == 6) bool = 0;
+	int bool = 1; //bool initially set to 1 
+	if(x->numConnections == 6) bool = 0; //if the provided room has 6 connections, it is maxed out
 	return bool;
 }
 
@@ -120,18 +127,12 @@ int CanAddConnectionFrom(struct Room* x){
 int ConnectionAlreadyExists(struct Room* x, struct Room* y){
 	int i;
 	int bool = 0;
-	printf("ConnectionAlreadyExists(): Test Room X Name: %s\n", x->roomName);
-	printf("Test Room Y Name: %s\n", y->roomName);
-	printf("Room X # Connections: %d\n", x->numConnections);
+	//will iterate for the Room X's # of connections
 	for(i = 0; i < x->numConnections; i++){
-		printf("Call one\n");
-		if(x->connections[i]->roomName ==  y->roomName) bool = 1;
-		printf("Call two\n");
+		//if one of the connections has the same name as Room Y, a connection exists, bool = 1
+		if(x->connections[i]->roomName ==  y->roomName) bool = 1; 
 	}
 	
-
-	printf("Returning connectexists: %d\n\n", bool);	
-
 	return bool;
 }
 
@@ -139,23 +140,21 @@ int ConnectionAlreadyExists(struct Room* x, struct Room* y){
  Function to connect two rooms when called on by AddRandomConnections()
 */
 void ConnectRoom(struct Room* x, struct Room* y){
-	int i, j, k, l;
+	int i, j;
+	//iterate up to seven times to find the room with the argument room name
 	for(i = 0; i < 7; i++){
 		if(rooms[i].roomName == x->roomName){
+		//once argument name is found, will iterate up to 6 times to find an empty slot
 			for(j = 0; j < 6; j++){
+				//room connection will be made in empty slot
 				if(rooms[i].connections[j] == NULL){
-					rooms[i].connections[j] = y;
-					rooms[i].numConnections++;
-					//rooms[i].connections[j]->numConnections++;
-					k = i;
-					l=j;
+					rooms[i].connections[j] = y; //slot set to room Y
+					rooms[i].numConnections++; //connection incremented
 					break;
 				}
 			}
 		}
 	}
-	printf("Connected:\n");
-	printf("%s with %s\n\n", rooms[k].roomName, rooms[k].connections[l]->roomName);
 }
 
 /*
@@ -164,42 +163,48 @@ void ConnectRoom(struct Room* x, struct Room* y){
  */
 int IsSameRoom(struct Room* x, struct Room* y){
 	int bool = 0;
+	//identifies if X and Y have the same room name by comparing string values
 	if(x->roomName == y->roomName) bool = 1;
 	
 	return bool;
 }
 
 int main(){
-	srand(time(NULL));
+	srand(time(NULL)); //set seed
 
 	int i, index;
 	int bool = 1;
+	//iterate room selections for up to 7 rooms
 	for(i = 0; i < 7; i++){
 		while(bool){
-			index = rand() % 10;
-			if(roomUsed(roomNames[index])==0) bool = 0;
+			index = rand() % 10; //randomize an index value between 0 ~ 9
+			if(roomUsed(roomNames[index])==0) bool = 0; //verifies if it has been used
 		}
+		
+		rooms[i].roomName = roomNames[index]; //set room name to randomly selected room
+		rooms[i].numConnections = 0; //initialize numConnections to 0
 
-		rooms[i].roomName = roomNames[index];
-		rooms[i].numConnections = 0;
-
-		if(i == 0) rooms[i].roomType = "START_ROOM";
-		else if(i == 6) rooms[i].roomType = "END_ROOM";
-		else rooms[i].roomType = "MID_ROOM";
+		if(i == 0) rooms[i].roomType = "START_ROOM"; //set beginning room to START_ROOM
+		else if(i == 6) rooms[i].roomType = "END_ROOM"; //set final room to END_ROOM
+		else rooms[i].roomType = "MID_ROOM"; //all other rooms set to MID_ROOM
 		
 		bool = 1;
 	}
-	int j;
-	for(j = 0; j < 7; j++){
-		printf("Room: %d | Name: %s | Type: %s\n\n", j + 1, rooms[j].roomName, rooms[j].roomType);	
-	}	
 	
-		
+	//continue to add rooms while graph is not full
 	while(IsGraphFull() == 0){
-		printf("adding connections\n\n");
 		AddRandomConnection();
-
-		printf("iterating again\n\n");
 	}
+	
+	int p, m;
+	for(p = 0; p < 7; p++){
+		printf("Room Name: %s\n", rooms[p].roomName);
+		printf("Room Type: %s\n", rooms[p].roomType);
+		for(m = 0; m < rooms[p].numConnections; m++){
+			printf("Connected: %s\n", rooms[p].connections[m]->roomName);
+		}
+		printf("\n");
+	}
+	
 	return 0;
 }
